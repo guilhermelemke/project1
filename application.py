5d9ac7d3de7de11ff940a3028343d5a984c21e32
guilhermelemke <guilherme.lemke@gmail.com>
2020-05-15 17:48:14 -0300

    Updates 15052020

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -23,4 +23,26 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    return render_template('index.html')
+
+@app.route("/search", methods=['POST'])
+def search():
+    name = request.form.get('username')
+    password = request.form.get('password')
+    if db.execute("SELECT * FROM users WHERE users.user = :name AND users.password = :password", {"name": name, "password": password}).rowcount == 1:
+        return render_template("search.html", username=name)
+    return render_template('error.html')
+
+@app.route("/registuser", methods=['GET'])
+def registuser():
+    return render_template('register.html')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    name = request.form.get('username')
+    password = request.form.get('password')
+    if db.execute("SELECT * FROM users WHERE users.user = :name", {"name": name}).rowcount == 1:
+        return render_template('error.html')
+    db.execute("INSERT INTO users (user, password) VALUES (:name, :password)", {"name": name, "password": password})
+    db.commit()
+    return render_template("search.html", username=name)
